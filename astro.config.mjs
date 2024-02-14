434e6313d1b69ca05a9a29fc8fe915454b4a5e3a
LorenzoTorchio <lorenzotorchios@gmail.com>
2024-02-14 17:46:47 -0300

    revert base on astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lorenzotorchio.github.io',
-  base: '/Portfolio',
+  site: 'https://lorenzotorchio.github.io/Portfolio',
   integrations: [mdx(), sitemap(), tailwind()]
 });
