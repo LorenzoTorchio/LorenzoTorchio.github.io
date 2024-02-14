029ecc2df72d08c856aca7a5f4e51d3b2ae59670
LorenzoTorchio <lorenzotorchios@gmail.com>
2024-02-14 17:00:48 -0300

    added base to astro.config.mjs (no styles are applying)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lorenzotorchio.github.io/Portfolio/',
+  site: 'https://lorenzotorchio.github.io',
+  base: '/Portfolio'
   integrations: [mdx(), sitemap(), tailwind()]
 });
